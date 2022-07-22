0aa94266c19583b7abac37fdfa920d4fb454abda
James Dawson <jamesdawsonWD@gmail.com>
2022-07-22 11:02:30 +0100

    new build

--- app/src/views/ClientProjects.vue
+++ app/src/views/ClientProjects.vue
@@ -26,7 +26,7 @@ const projects: Project[] = [
     modal: ModalTypes.ProjectInfo,
     description:
       "Liquid Finance is a collection of smart contracts that allowed for Liquidity Mining of Synthetic Tokens using the UMA protocol. I designed and developed all the smart contracts that passed a suite of tests that I also made. This was my first project working with auditors and I was very happy to of passed the audit. I also designed and developed the entire frontend. The app allows the user to trade, stake, create bonds and participate in the governance of the protocol all through the app. This is currently a mock of the applicaiton and the client has not yet released their product onto mainet.",
-    url: "",
+    url: "https://liquid-finance-kovan.herokuapp.com/",
   },
 ];
 </script>
